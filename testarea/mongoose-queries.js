33c331c217bd05ef02bdbbd35e1f4423a40fcc51
hmarsolla <hmarsolla@hotmail.com>
2018-01-10 01:45:49 -0200

    Added DELETE /todos/:id

--- testarea/mongoose-queries.js
+++ testarea/mongoose-queries.js
@@ -1,6 +1,7 @@
 const {ObjectID} = require("mongodb");
 const {mongoose} = require("./../server/db/mongoose");
 const {Todo} = require("./../server/models/todo");
+const {User} = require("./../server/models/user");
 
 var id = "6a5256ba7296603bc88b290f";
 
