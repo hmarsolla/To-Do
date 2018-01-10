dddff716f1d8b9c070e1b6519e401d1cad4b0fdd
hmarsolla <hmarsolla@hotmail.com>
2018-01-09 23:22:44 -0200

    Setup config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require("mongoose");
+var {config} = require("./../config.js");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp", { useMongoClient: true });
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp", { useMongoClient: true });
+mongoose.connect(config.connectionString, { useMongoClient: true });
+
 
 module.exports = {mongoose};
