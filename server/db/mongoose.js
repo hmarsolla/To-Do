076183cab28d0980157beb6e2fc23ae7082336ad
hmarsolla <hmarsolla@hotmail.com>
2018-01-11 02:21:38 -0200

    Set up database environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,6 @@
 var mongoose = require("mongoose");
-var {config} = require("./../config.js");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp", { useMongoClient: true });
-//mongoose.connect(config.connectionString, { useMongoClient: true });
-
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
 
 module.exports = {mongoose};
